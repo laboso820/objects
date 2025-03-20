a648682240df77eeaa356009dc869ee497c1a7c1
laboso820 <obadiahlaboso820@gmail.com>
2025-03-20 22:15:41 +0300

    new changes

---
+++ main.js
@@ -0,0 +1,15 @@
+// myname ="Oba"
+// console.log(myname)
+
+const teacher={
+    name:"james",
+    age:23,
+    subject: "maths",
+}
+const animal ={
+     dog:2
+}
+teacher.subject = "english"  
+console.log(teacher.name)
+console.log(teacher.subject)
+console.log(animal.dog)
