98a74925f6f44c4528aa927f9e13f97321239256
laboso820 <obadiahlaboso820@gmail.com>
2025-03-22 23:04:39 +0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,20 @@
+console.log("this is from arrays")
+//  const shoppingList =["buy potatoes", "o to butchery"]
+const teachers=[
+{
+    name:"john",
+    class: "F2",
+    subject: "maths",
+},
+{
+    name:"ogweno",
+    class: "F3",
+    subject: "physics",
+},
+{
+    name:"mercy",
+    class: "F4",
+    subject: "english",
+}
+]
+console.log(teachers[1])
