a2a7e37cf9ad79cb93b109205d641a0c1540fa0a
laboso820 <obadiahlaboso820@gmail.com>
2025-03-23 20:12:52 +0300

    functions

--- functions.js
+++ functions.js
@@ -6,3 +6,20 @@ function sayHello(){
     console.log("how was your day")
 }
 sayHello()
+const balance=200;
+const user1={
+    name:"obadiah",
+    balance:2000,
+    account:"thebee",
+}
+const user2={
+    name:"Laboso",
+    balance:2000,
+    account:"1234",
+}
+function withdraw(){
+    if(balance<0){
+        console.log("you have insufficient funds")
+    }
+}
+withdraw(user1)
