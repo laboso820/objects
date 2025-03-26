0898c12adfedeedf232909ed872fca925d056732
laboso820 <obadiahlaboso820@gmail.com>
2025-03-26 22:13:42 +0300

    mode

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 console.log("this is from loop file")
 let fruits =["Mangoes,","oranges","banana"]
 
-for( fruits=0; fruits<20; fruits++){
+for( fruits=0; fruits<=20; fruits++){
     console.log(fruits)
+
 }
