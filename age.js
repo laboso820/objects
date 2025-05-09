9fc76afd6815cbcb855ca44953974f492d305a0e
laboso820 <obadiahlaboso820@gmail.com>
2025-05-09 19:05:42 +0300

    age file added

---
+++ age.js
@@ -0,0 +1,13 @@
+console.log("this is from age javascript file")
+let age =30
+
+if(age<=10){
+    console.log("youre under age")
+}
+else if(age==20){
+    console.log("you can enroll")
+
+}
+else{
+    console.log("youre old to enroll")
+}
--- loops.js
+++ loops.js
@@ -1,7 +1,23 @@
 console.log("this is from loop file")
-let fruits =["Mangoes,","oranges","banana"]
+function fruits ([Mangoes, oranges,banana]){
 
-for( fruits=0; fruits<=20; fruits++){
-    console.log(fruits)
+    if (fruits=[0]){
+    console.log("Mangoes")
+}
+else if(fruits=[1]){
+    console.log("oranges")
 
 }
+else{
+    console.log("banana")
+}
+}
+
+// for( fruits=0; fruits<=20; fruits++){
+//     console.log(fruits.length)
+
+// }
+// console.log(fruits.length)
+// console.log(typeof fruits)
+
+fruits([2])
